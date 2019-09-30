68db79dbaafac06ae5c4c44a6477e7197088bd56
Winifred Frierson <winifred.dykes@hotmail.com>
2019-09-30 02:27:55 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,22 @@
 def find_element_index(array, value_to_find)
-  
+  require 'pry'
+
+describe 'Finding data' do
+
+  scale = [2,3,4,5]
+  scale_2 = [6,5,4,3]
+  hill = [1,2,3,4,5,4,3,2,1]
+  valley = [5,4,3,2,1,0,1,2,3,4,5]
+  zig_zag = [500,4,1000,5,250]
+
+  describe 'find_element_index(array, value_to_find)' do
+    it 'takes in an array and a value and returns the index of that value' do
+      expect(find_element_index(scale,2)).to eq(0)
+      expect(find_element_index(scale,4)).to eq(2)
+      expect(find_element_index(scale_2,5)).to eq(1)
+      expect(find_element_index(scale_2,6)).to eq(0)
+    end
+
   # Add your solution here
 end
 
